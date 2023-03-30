eabc606fc28129f05ef398e0983ffd29424a1a05
🤦🏾‍♂️ 😑 <K1NGSPIFF@outlook.com>
2023-03-29 21:09:57 -0500

    java commit

---
+++ lc101_pt_webdev/java-practice/java-exercises/lsn1-exercises/src/main/java/lsn1/excercises/Rectangle.java
@@ -0,0 +1,18 @@
+package lsn1.exercises;
+import java.util.Scanner;
+public class Rectangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("What is the length of the rectangle?");
+         Double length = input.nextDouble();
+        
+        System.out.println("What is the width of the rectangle?");
+        Double width = input.nextDouble();
+
+        Double area = (width * length);
+        System.out.println("The area of the rectangle is: " + area);
+
+
+    }
+}
