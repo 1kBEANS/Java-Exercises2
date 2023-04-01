7cfb09c178a8ade2117de83ff6cb028f183124cf
🤦🏾‍♂️ 😑 <K1NGSPIFF@outlook.com>
2023-04-01 12:42:04 -0500

    committing intArrayList

---
+++ lc101_pt_webdev/java-practice/java-exercises/Java-Excercise2/src/main/java/array/list/practice/integerArrayList.java
@@ -0,0 +1,8 @@
+package array.list.practice;
+import java.util.ArrayList;
+public class integerArrayList {
+    public static void main(String[] args) {
+          ArrayList<Integer> intArrayList = new ArrayList<>();
+          int[] list = {1, 3, 5, 6, 7, 9, 8, 4, 2, 0};
+    }
+}
